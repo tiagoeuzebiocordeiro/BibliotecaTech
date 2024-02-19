2efb0063334910398d74689f8966c84ad16988f3
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-02-19 16:48:43 -0300

    Adicao de link para Editora na navBar, criacao de Index (table view) de editora e Create de Editora

---
+++ BibliotecaTech/Controllers/EditoraController.cs
@@ -0,0 +1,38 @@
+﻿using BibliotecaTech.Data;
+using BibliotecaTech.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaTech.Controllers
+{
+    public class EditoraController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EditoraController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EditoraModel> editoras = _context.Editoras;
+            return View(editoras);
+        }
+
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EditoraModel editora)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Editoras.Add(editora);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
